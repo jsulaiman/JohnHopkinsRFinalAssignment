7121a38625e54fe7a664483699894c61f3b55149
jsulaima <lai.s.johan@gmail.com>
2016-03-17 08:14:25 -0700

    Added if statements for the three outcomes.

--- best.R
+++ best.R
@@ -1,9 +1,21 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  bestRawData <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available",stringsAsFactors=FALSE)
   ## Check that state and outcome are valid
   
+  ## Return hospital name in that state with lowest 30-day deathrate
   
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  if (outcome=="heart attack"){
+    print("ha")
+  }
+  
+  if (outcome=="heart failure") {
+    print("hf")
+  }
+    
+  if (outcome=="pneumonia") {
+    print("p")
+  }
+
+  print(head(bestRawData$Hospital.Name))
 }
