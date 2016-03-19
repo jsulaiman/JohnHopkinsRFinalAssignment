d730fbc7e766029b9376f45a0433e43582642e89
jsulaima <lai.s.johan@gmail.com>
2016-03-19 08:29:40 -0700

    best Function complete

--- best.R
+++ best.R
@@ -6,16 +6,24 @@ best <- function(state, outcome) {
   ## Return hospital name in that state with lowest 30-day deathrate
   
   if (outcome=="heart attack"){
-    print("ha")
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,11))
+    
   }
   
   if (outcome=="heart failure") {
-    print("hf")
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,17))
   }
     
   if (outcome=="pneumonia") {
-    print("p")
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,23))
   }
 
-  print(head(bestRawData$Hospital.Name))
+  names(bestSubset)<-c("Name","DesiredState","DesiredOutcome")
+
+  bestSubset<-as.data.frame(na.omit(bestSubset))
+  bestSubset<-bestSubset[bestSubset[,"DesiredState"]==state,]
+  str(bestSubset)
+  sorted<-bestSubset[order(bestSubset$DesiredOutcome),]
+  str(sorted)
+  print(head(sorted,5))
 }
