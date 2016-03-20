95063b090d49d5ec2a65fea0e088d957dc342f2b
jsulaima <lai.s.johan@gmail.com>
2016-03-20 08:02:16 -0700

    Final codes to complete John Hopkins Final Assigments

--- best.R
+++ best.R
@@ -27,3 +27,50 @@ best <- function(state, outcome) {
   str(sorted)
   print(head(sorted,5))
 }
+
+rankhospital<-function(state,outcome,num = "best") {
+  ## Read outcome data
+  bestRawData <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available",stringsAsFactors=FALSE)
+  ## Check that state and outcome are valid
+  
+  ## Return hospital name in that state with lowest 30-day deathrate
+  
+  if (outcome=="heart attack"){
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,11))
+    
+  }
+  
+  if (outcome=="heart failure") {
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,17))
+  }
+  
+  if (outcome=="pneumonia") {
+    bestSubset<-subset(bestRawData,select= c(Hospital.Name,State,23))
+  }
+  
+  names(bestSubset)<-c("Name","DesiredState","DesiredOutcome")
+  
+  bestSubset<-as.data.frame(na.omit(bestSubset))
+  
+  bestSubset<-bestSubset[bestSubset[,"DesiredState"]==state,]
+
+  names(bestSubset)<-c("Name","DesiredState","DesiredOutcome")
+  sorted<-bestSubset[order(bestSubset$DesiredOutcome,bestSubset$Name),]
+  
+  RankVector<-1:nrow(sorted)
+  sorted<-cbind(sorted,RankVector)
+  
+  names(sorted)<-c("Name","DesiredState","DesiredOutcome","Rank")
+  
+  print(sorted)
+
+  RankSelection=which.min(sorted$Rank)
+  
+  if (num == "worst")  {
+    RankSelection=which.max(sorted$Rank)
+  } 
+  else if (num != "best") {
+   RankSelection=num
+  }
+  print(sorted[sorted[,"Rank"]==RankSelection,])
+}
